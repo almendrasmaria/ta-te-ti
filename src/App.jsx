f84fd6819109a936092406fa9f638241da42fa70
nahudev01 <garcianahuel77@gmail.com>
2025-07-23 22:24:25 -0300

    add function checkWinner y refactor validacion para evitar movimientos en campos ocupados

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ function App() {
 
   const updateBoard = (index) => {
 
-    if (board[index] === "X" || board[index] === "O") {
-    return;
-    }
+    if (board[index] || checkWinner(board)) return;
 
     const newBoard = [...board];
     newBoard[index] = turn;
@@ -18,6 +16,22 @@ function App() {
     const newTurn = turn === "X" ? "O" : "X"
     setTurn (newTurn)
   }
+
+  const checkWinner = (board) => {
+    const winningCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6],
+    ];
+
+    for (let combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  };
 
   return (
   
