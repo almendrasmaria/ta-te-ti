7941f63bf3422f3ea348a2b0ee61cde45ec41c64
nahudev01 <garcianahuel77@gmail.com>
2025-07-25 18:34:15 -0300

    Refactor modulamos checkWinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,8 @@
 import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRepeat } from '@fortawesome/free-solid-svg-icons';
+import { checkWinner } from './checkWinner';
 import './App.css'
-
-const winCombos = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8], 
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
-
-const checkWinner = (board) => {
-  for (let combo of winCombos) {
-    const [pos1,pos2,pos3] = combo; 
-    if (
-      board[pos1] && board[pos1] === board [pos2] && board [pos1] === board [pos3]
-    ) {
-      return board [pos1]
-    }
-  }
-  return null;
-};
 
 function App() {
   const [board, setBoard] = useState (Array(9).fill(null));
